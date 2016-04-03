f5a3b1789584806868681416cb13f9f79990a6af
premek <premysl.vyhnal@gmail.com>
2016-04-03 17:01:38 +0200

    engine sfx

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,5 @@
 local Signal = require 'lib.hump.signal'
+local Timer = require 'lib.hump.timer'
 local Gamestate = require "lib.hump.gamestate"
 
 local state = {
@@ -15,15 +16,14 @@ local music
 local img = {}
 
 local a=64 -- pixels
-
-local scale = 10
+local scale = 8
 
 local fuel = 1
 local speed = 0
-local accel = 80
-local friction = 60
+local accel = 45
+local friction = 37
 local maxspeed = 80
-local consumption = .001
+local consumption = .005
 local distance = 0
 
 local palette = {
@@ -52,6 +52,12 @@ local texts = {
 }
 local sortedTextsKeys = {}
 
+local sfx = {}
+
+
+--------------
+
+
 local getQuads = function(image)
   local quads={}
   for i=0,image:getWidth()/a do
@@ -72,7 +78,13 @@ function love.load()
   music = love.audio.newSource( 'music/Stunts (1990) - Soundtrack.mp3', 'stream' )
   music:setLooping( true )
   music:setVolume(.7)
-  music:play()
+  --music:play()
+
+  for _,v in ipairs(love.filesystem.getDirectoryItems("sfx")) do
+    sfx[v:match("^(.+)%..+$")] = love.audio.newSource("sfx/"..v, "static")
+  end
+  sfx.idle:setLooping(true)
+  sfx.going:setLooping(true)
 
   for _,f in ipairs({"bg1", "fuel", "road", "car", "sign"}) do
     img[f] = {}
@@ -92,8 +104,9 @@ function love.load()
 
   Gamestate.registerEvents()
   Gamestate.switch(state.driving)
-  Gamestate.push(state.reading)
+  --Gamestate.push(state.reading)
 
+  Signal.emit('game_started') -- TODO after main menu?
 end
 
 
@@ -101,14 +114,26 @@ function love.draw()
   love.graphics.scale(scale)
 end
 
---------------
+function love.update(dt)
+  Timer.update(dt)
+end
 
+
+
+-------------- driving
+local lastgas = false
 
 function state.driving:update(dt)
   local origspeed = speed
-  if love.keyboard.isDown("up") and fuel > 0 then
+  local gas = love.keyboard.isDown("up") and fuel > 0
+  if gas then
     speed = speed + dt*accel
+    if not lastgas then Signal.emit('accel_start') end
+  else
+    if lastgas then Signal.emit('accel_end') end
   end
+  lastgas = gas
+
   speed = speed - dt*friction -- FIXME ???
   speed = math.max(math.min(maxspeed, speed), 0)
 
@@ -148,7 +173,9 @@ end
 
 
 
---------------
+
+
+-------------- reading
 function state.reading:draw()
   love.graphics.setColor(palette[3])
   love.graphics.rectangle("fill", 0, 0, a, a)
@@ -163,23 +190,73 @@ function state.reading:keypressed(key)
   if key=='up' then Gamestate.pop() end
 end
 
------------
-
-function love.keypressed(key)
-  if key=='d' then debug = not debug end
-  if key=='escape' then love.event.quit() end
-  if key=='+' or key=='kp+' then scale = scale + 1; scaleWindow() end
-  if key=='-' or key=='kp-' then scale = scale - 1; scaleWindow() end
-end
-function love.textinput(text)
-  if text=='+' then scale = scale + 1; scaleWindow() end
-  if text=='-' then scale = scale - 1; scaleWindow() end
-end
-
 
 
 --------------
 
 Signal.register('stopped', function()
-  Gamestate.push(state.reading)
+  --Gamestate.push(state.reading)
 end)
+
+
+
+
+--- sfx
+
+local sfxTimers = {}
+local stopAllSfx = function()
+  for _,v in pairs(sfx) do v:stop() end
+  for _,v in pairs(sfxTimers) do Timer.cancel(v) end
+  sfxTimers = {}
+end
+
+Signal.register('game_started', function()
+  local delay = 1
+  table.insert(sfxTimers, Timer.after(delay, function() sfx.start:play() end))
+  table.insert(sfxTimers, Timer.after(delay + sfx.start:getDuration(), function() sfx.idle:play() end))
+end)
+
+Signal.register('accel_start', function()
+  stopAllSfx()
+  sfx.speedup:play()
+  table.insert(sfxTimers, Timer.after(sfx.speedup:getDuration(), function() sfx.going:play() end))
+end)
+
+Signal.register('accel_end', function()
+  if fuel > 0 then
+    stopAllSfx()
+    sfx.speeddown:play()
+    table.insert(sfxTimers, Timer.after(sfx.speeddown:getDuration(), function() sfx.idle:play() end))
+  end
+end)
+
+local stopPlayed = false
+Signal.register('out_of_fuel', function()
+  if not stopPlayed then
+    stopAllSfx()
+    sfx.stop:play()
+    stopPlayed = true
+  end
+end)
+
+Signal.register('stopped', function()
+  if(fuel>0) then
+    stopAllSfx()
+    sfx.idle:play()
+  end
+end)
+
+
+
+ -----controls
+
+ function love.keypressed(key)
+   if key=='d' then debug = not debug end
+   if key=='escape' then love.event.quit() end
+   if key=='+' or key=='kp+' then scale = scale + 1; scaleWindow() end
+   if key=='-' or key=='kp-' then scale = scale - 1; scaleWindow() end
+ end
+ function love.textinput(text)
+   if text=='+' then scale = scale + 1; scaleWindow() end
+   if text=='-' then scale = scale - 1; scaleWindow() end
+ end
